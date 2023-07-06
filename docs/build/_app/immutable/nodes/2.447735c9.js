e4f691965c21fad0114bb9f514d427a5c2228e17
Vaisakh K M <vaisakhkm2625@gmail.com>
2023-07-06 17:17:52 +0530

    fixed build: remove docs/* for each build

--- docs/build/_app/immutable/nodes/2.447735c9.js
+++
@@ -1,5 +0,0 @@
-import{s as C,n as h}from"../chunks/scheduler.e108d1fd.js";import{S as $,i as b,g as v,m as c,s as q,h as k,y,n as _,c as E,j as S,f as o,a as f,x as T,z as j,o as x}from"../chunks/index.7e6319f2.js";function z(n){let s,m="Equation solvers",a,i,p,l,r,u,d;return{c(){s=v("h1"),s.textContent=m,a=c(`
-
-vaisakh `),i=c(n[0]),p=q(),l=v("button"),r=c(n[0])},l(t){s=k(t,"H1",{["data-svelte-h"]:!0}),y(s)!=="svelte-6dkxl6"&&(s.textContent=m),a=_(t,`
-
-vaisakh `),i=_(t,n[0]),p=E(t),l=k(t,"BUTTON",{});var e=S(l);r=_(e,n[0]),e.forEach(o)},m(t,e){f(t,s,e),f(t,a,e),f(t,i,e),f(t,p,e),f(t,l,e),T(l,r),u||(d=j(l,"click",n[1]),u=!0)},p(t,[e]){e&1&&x(i,t[0]),e&1&&x(r,t[0])},i:h,o:h,d(t){t&&(o(s),o(a),o(i),o(p),o(l)),u=!1,d()}}}function B(n,s,m){let a=3;function i(){m(0,a++,a)}return[a,i]}class O extends ${constructor(s){super(),b(this,s,B,z,C,{})}}export{O as component};
